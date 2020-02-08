3c84f30d06e2efb0f607e4b235fb3f42b1485923
Albert Bertram <bertrama@umich.edu>
2020-02-07 21:18:38 -0500

    Add validate feature and tests

--- src/Readership/Map/DataValidator.php
+++ src/Readership/Map/DataValidator.php
@@ -2,5 +2,55 @@
 namespace Readership\Map;
 
 class DataValidator {
-  
+  private $pinAttributes = [
+    'title',
+    'author',
+    'view_id',
+    'url',
+    'access',
+    'date',
+    'position',
+    'location',
+  ];
+
+  public function validFile($file) {
+    $json = json_decode(file_get_contents($file), TRUE);
+    if ($this->validJSON($json)) {
+      return 0;
+    }
+    return 1;
+  }
+
+  public function validJSON($json) {
+    if (!is_array($json)) { return FALSE; }
+    if (empty($json['pageviews'])) { return FALSE; }
+    if (empty($json['pageviews']['total'])) { return FALSE; }
+    if (empty($json['pageviews']['annual'])) { return FALSE; }
+    if (!$this->validCounts($json['pageviews']['total'])) { return FALSE; }
+    if (!$this->validCounts($json['pageviews']['annual'])) { return FALSE; }
+    if (empty($json['pins'])) { return FALSE; }
+    return $this->validPins($json['pins']);
+  }
+
+  private function validPins($pins) {
+    foreach ($pins as $pin) {
+      foreach ($this->pinAttributes as $attr) {
+        if (!isset($pin[$attr])) {
+          return FALSE;
+        }
+      }
+      if (!isset($pin['position']['lat'])) { return FALSE; }
+      if (!isset($pin['position']['lng'])) { return FALSE; }
+    }
+    return TRUE;
+  }
+
+  private function validCounts($counts) {
+    foreach ($counts as $count) {
+      if (!isset($count['count']) || !isset($count['view_id'])) {
+        return FALSE;
+      }
+    }
+    return TRUE;
+  }
 }
--- tests/unit/Readership/Map/DataValidatorTest.php
+++ tests/unit/Readership/Map/DataValidatorTest.php
@@ -5,8 +5,26 @@ use Readership\Map\DataValidator;
 
 class DataValidatorTest extends TestCase {
 
-  public function testValidateData() {
-    $validator = new DataValidator();
-    $this->assertTrue(true);
+  protected function setUp() {
+    $this->dataDir = 'tests/data/Readership/Map/DataValidatorTest';
+    $this->validator = new DataValidator();
+    $this->validFile = $this->dataDir . '/validFile.json';
+    $this->invalidPins = $this->dataDir . '/invalidPins.json';
+    $this->invalidCounts = $this->dataDir . '/invalidCounts.json';
+  }
+
+  public function testValidFile() {
+    $result = $this->validator->validFile($this->validFile);
+    $this->assertSame($result, 0);
+  }
+
+  public function testInvalidPins() {
+    $result = $this->validator->validFile($this->invalidPins);
+    $this->assertSame($result, 1);
+  }
+
+  public function testInvalidCounts() {
+    $result = $this->validator->validFile($this->invalidCounts);
+    $this->assertSame($result, 1);
   }
 }
